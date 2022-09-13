aa4490b4223ee1df6e85696821a57417c8bb7811
Bhumireddy Kalyanreddy <bkalyanreddy@manh.com>
2022-09-13 14:38:52 +0530

    Initial commit

---
+++ OOPS.py
@@ -0,0 +1,11 @@
+class kalyan:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def name(self):
+        return self.name
+    def age(self):
+        return self.age
+kalyan1=kalyan("kalyan",22)
+print(kalyan1.name)
+print(kalyan1.age)
